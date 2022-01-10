f302d580499c0d4a5650809136bddf1376fcc90d
Jack Sawyer <jack.h.sawyer@pm.me>
2022-01-10 08:04:10 -0600

    first commit

---
+++ arrays.java
@@ -0,0 +1,28 @@
+class arrays {
+    public static void main(String[] args) {
+
+        int[] myArray;
+        myArray = new int[5];
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            myArray[0] = 4;
+            myArray[1] = 8;
+            myArray[2] = 8;
+            myArray[3] = 5;
+            myArray[4] = 9;
+
+            System.out.println(myArray[i]);
+        }
+
+        String[] fruits= {"banana", "pear", "papaya", "kiwi"};
+        String temp = fruits[0];
+        fruits[0] = fruits[fruits.length - 1];
+        fruits[fruits.length - 1] = temp;
+
+        for (int i = 0; i < fruits.length; i++) {
+
+            System.out.println(fruits[i]);
+        }
+    }
+}
